307aefa11671d5fd99eca6ac8fdad441b1e3978c
it-me-kkaran11 <rkarunakaran174@gmail.com>
2026-08-25 22:36:47 +0530

    Environmental variable issue is fixed and ready to commit changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+const projectRoot = fileURLToPath(new URL('.', import.meta.url));
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: projectRoot,
+  envDir: projectRoot,
   plugins: [react()],
   resolve: {
     alias: {
